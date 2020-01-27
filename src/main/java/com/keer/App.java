4d8d6e741f1fba630dd804e26390db77ba8ca97c
keer123456789 <15110074528@163.com>
2020-01-27 20:43:58 +0800

    feat: 🎸 init new project

---
+++ src/main/java/com/keer/App.java
@@ -0,0 +1,13 @@
+package com.keer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
